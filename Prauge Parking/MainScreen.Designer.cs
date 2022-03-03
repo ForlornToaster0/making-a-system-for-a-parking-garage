eecd3cce5f1a466cbc8bd5878a60b74d48cae6c4
samuel andersson <samuel.ander@hotmail.com>
2022-03-03 12:10:36 +0100

    fixed connetctionstring

--- Prauge Parking/MainScreen.Designer.cs
+++ Prauge Parking/MainScreen.Designer.cs
@@ -28,25 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(328, 376);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Name = "MainScreen";
             this.Text = "MainScreen";
             this.ResumeLayout(false);
@@ -54,6 +42,5 @@
         }
 
         #endregion
-        private Button button1;
     }
 }
