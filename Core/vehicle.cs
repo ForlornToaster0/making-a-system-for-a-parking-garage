219b80f49b610d6620742f5a0dc66af1f39daace
samuel andersson <samuel.ander@hotmail.com>
2022-03-04 13:20:05 +0100

    added vechial OOP

---
+++ Core/vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class vehicle
+    {
+        public string? RegNumber { get; set; }
+        public string? VehicleType { get; set; }
+        public DateTime ParkTime { get; set; }
+        public int ParkedOnSpot { get; set; }
+    }
+    public class Car : vehicle
+    {
+        public int VehicleCost { get; set; }
+        public int VehicleSize { get; set; }
+    }
+    public class MC : vehicle
+    {
+        public int VehicleCost { get; set; }
+        public int VehicleSize { get; set; }
+    }
+}
